1c4c90388ece85f375cf4f02fe1ef7230a7f8df1
Vinícius Yoda <vyoda4604@gmail.com>
2023-05-29 13:52:01 -0300

    type-string

---
+++ javascript-1/type-string.js
@@ -0,0 +1,13 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!"
+const StringDeNumero = "34567";
+
+const citacao = 'Meu nome é ';
+const meuNome = "Vinícius";
+
+// concatenação (+)
+
+console.log(citacao + meuNome)
+
+// template string ou template literal
