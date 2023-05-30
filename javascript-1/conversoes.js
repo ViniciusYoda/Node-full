ca847da1bab509ff7538278c727b95573c0be10b
Vinícius Yoda <vyoda4604@gmail.com>
2023-05-30 12:49:53 -0300

    passo 2

---
+++ javascript-1/conversoes.js
@@ -0,0 +1,14 @@
+// tipo de dado
+// booleanos
+
+// conversão implicita
+const numero = 456;
+const numeroString = Number("456");
+
+console.log(numero == numeroString)
+
+// Number()
+// String()
+console.log(numero + numeroString);
+
+// conversão explicita
